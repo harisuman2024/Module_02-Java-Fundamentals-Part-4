96349458d7afb351eb8ad540a197c159d72543de
HARI SUMAN <95356493+harisuman2024@users.noreply.github.com>
2024-10-04 10:22:19 +0530

    Module 2: Part 4
    from control statements ..........

---
+++ Lab156.java
@@ -0,0 +1,19 @@
+class Lab156 {
+    public static void main(String jlc[]) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
+
+// 1 x
+// 2 
+// 3 x
+// 4 
+// 5  x
+// 6 
+// 7 x
+// 8 
+// 9  x
+// 10 
