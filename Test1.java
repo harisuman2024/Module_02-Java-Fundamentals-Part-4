fea839139ee13c134c91603d74e9549afc4a1192
HARI SUMAN <95356493+harisuman2024@users.noreply.github.com>
2024-10-06 20:58:15 +0530

    Test 4  Lab
    Test 4  all Labs

---
+++ Test1.java
@@ -0,0 +1,11 @@
+class Test1 {
+    public static void main(String[] args) {
+        boolean b = false;
+        if (b = true) {
+            System.out.println("IF");
+        } else 
+             {
+                  System.out.println("ELSE");
+             }
+    }
+}
---
+++ Test18.java
@@ -0,0 +1,7 @@
+class Test18{
+public static void main(String as[]){
+for(int i=1;i<2;i++){
+}
+System.out.println("OK");
+}
+} 
